2ad0619e883f6d82bd3e74b54494a4559d13b5b6
Cattatonicat <threeeyedvenelope@gmail.com>
2021-05-11 18:39:23 -0500

    Display player

--- SFML2D4DirectionMotion/Source.cpp
+++ SFML2D4DirectionMotion/Source.cpp
@@ -1,10 +1,13 @@
 #include <SFML/Graphics.hpp>
+#include "Player.h"
 
 sf::Vector2f windowSize(800, 800);
 sf::VideoMode vm(windowSize.x, windowSize.y);
 sf::RenderWindow window(vm, "2D 4 Direction Movement");
 
 int main() {
+	Player player("graphics/AngryHeartx8.png", sf::Vector2f(windowSize.x / 2, windowSize.y / 2));
+
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
@@ -12,6 +15,10 @@ int main() {
 				window.close();
 			}
 		}
+
+		window.clear();
+		window.draw(player.getSprite());
+		window.display();
 	}
 	return 0;
 }
